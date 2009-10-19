07b69aa70ea6947918ad9a0cb74561ac56083e6c
colinpmillar (Mr ) <colin.millar@jrc.ec.europa.eu>
2009-10-19 13:03:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/selectivity@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -18,182 +18,29 @@
 </head>
 
 <body>
-<!-- wrap starts here -->
-<div id="wrap">
 
-	<!--header -->
-	<div id="header">
-
-		<h1 id="logo-text"><a href="index.php" title="">FRS Stats</a></h1>
-		<p id="slogan">R package of Fishing Gear Selectivity Analysis Functions</p>
-
-		<form id="quick-search" action="index.php" method="get" >
-			<p>
-			<label for="qsearch">Search:</label>
-			<input class="tbox" id="qsearch" type="text" name="qsearch" value="Search..." title="Start typing and hit ENTER" />
-			<input class="btn" alt="Search" type="image" name="searchsubmit" title="Search" src="images/search.gif" />
-			</p>
-		</form>
-
-	<!--header ends-->
-	</div>
-
-	<!-- navigation starts-->
-	<div  id="nav">
-		<ul>
-			<li id="current"><a href="index.php">Home</a></li>
-			<li><a href="http://cran.r-project.org/">CRAN</a></li>
-			<li><a href="http://r-forge.r-project.org/">R-Forge</a></li>
-			<li><a href="http://www.marlab.ac.uk/">FRS Homepage</a></li>
-			<li><a href="index.php">About</a></li>
-			<li><a href="index.php">Contact</a></li>
-		</ul>
-	<!-- navigation ends-->
-	</div>
-
-	<!-- content-wrap starts -->
-	<div id="content-wrap">
-
-		<div id="main">
-
-			<a name="Welcome"></a>
-			<h2><a href="index.php" title="">Welcome to the Selectivity project on R-Forge!</a></h2>
-
-			<p>Some information on whatwe are trying to achieve witht this package...</p>
+<! --- R-Forge Logo --- >
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="http://r-forge.r-project.org/themes/rforge//images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+</table>
 
 
-			<h2><a href="index.php">plot survey index correlations</a></h2>
+<!-- get project title  -->
+<!-- own website starts here, the following may be changed as you like -->
 
-			<h3>Plots - say what it does</h3>
-			<p><a href="http://getfirefox.com/"><img src="images/catch_correlations.png" width="100" height="120" alt="catch correlations" class="float-left" /></a>
-			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
-			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
-			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum
-			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra
-			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna.
-			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
-			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
-			posuere nunc justo tempus leo.
-			</p>
+<h2>Welcome to lme4 - Mixed-effects models project!</h2>
 
-			<blockquote>
-			<p>Something interesting form someone equally interesting</p>
-			<p class="align-right"> - Jimmy Beaver</p>
-			</blockquote>
+<p>An R package for fitting and analyzing linear, nonlinear and generalized linear mixed models.
+<!-- end of project description -->
 
-			<p>
-			Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit amet bibendum
-			pellentesque, ante sapien varius turpis, eu nonummy nunc urna scelerisque ante.
-			Nunc sed leo at dolor pharetra facilisis. Sed sed lectus at justo dapibus tristique.
-			Morbi molestie pulvinar elit. Donec dictum nibh nec est. Aliquam sagittis, lacus
-			a pharetra tempor, enim justo tempor nisi, at aliquet nulla metus sed augue.
-			Praesent sed nibh. Vestibulum bibendum enim ut nisi fringilla accumsan.
-			Nullam dapibus fermentum nulla. Phasellus in orci vel justo facilisis iaculis.
-			Quisque facilisis ornare felis. Curabitur nunc libero, dapibus vel, commodo nec,
-			semper a, massa. Suspendisse potenti. Morbi a nisl. Nunc euismod elit non lectus.
-			</p>
+<p> <strong>Doxygen</strong> documentation of the underlying C functions is <a href="./doxygen/"><strong>here</strong></a>. </p>
 
-			<h2><a href="index.php">Another plot</a></h2>
+<p> The <strong>project summary page</strong> you can find <a href="http://r-forge.r-project.org/projects/lme4/"><strong>here</strong></a>. </p>
 
-			<h3>Say what this one does</h3>
-			<p><a href="http://getfirefox.com/"><img src="images/firefox-gray.jpg" width="100" height="120" alt="firefox" class="float-right" /></a>
-			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
-			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
-			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum
-			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra
-			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna.
-			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
-			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
-			posuere nunc justo tempus leo.
-			</p>
+<p> <strong>References to articles and other research using nlme or lme4</strong> can be found <a href="bib/lme4bib.html"><strong>here</strong></a>. The LaTeX bibliography file can be accessed from <a href="bib/lme4bib.bib">here</a>. If you would like to add your work to this database, please email vasishth.shravan at gmail dot com</p>
 
-		<!-- main ends -->
-		</div>
-
-		<div id="sidebar">
-
-			<h3>FRS Stats Links</h3>
-			<ul class="sidemenu">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="http://r-forge.r-project.org/projects/ices-frs/">FRS Stats R-Forge site</a></li>
-				<li><a href="http://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/?root=ices-frs">Browse FRS Stats Subversion Repository</a></li>
-			</ul>
-
-			<h3>Search Box</h3>
-			<form class="searchform" action="index.php" method="get">
-				<p>
-				<input name="search_query" class="textbox" type="text" />
-  				<input name="search" class="button" value="Search" type="submit" />
-				</p>
-			</form>
-
-			<h3>Wise Words</h3>
-			<p>&quot;Good judgement comes from experience; and experience comes from
-               bad judgement.&quot; </p>
-
-			<p class="align-right">- Mark Twain</p>
-
-			<h3>Support Styleshout</h3>
-
-      <p>This page was based on a design by <a href="http://www.styleshout.com/">Styleshout</a>.
-      If you are interested in supporting the work of Styleshout and would like to contribute, you are
-			welcome to make a small donation through the
-			<a href="http://www.styleshout.com/">donate link</a> on the Styleshout website - it will
-			be a great help and will surely be appreciated.</p>
-
-		<!-- sidebar ends -->
-		</div>
-
-	<!-- content-wrap ends-->
-	</div>
-
-	<!-- footer starts -->
-	<div id="footer-wrap"><div id="footer-content">
-
-		<div id="footer-columns">
-			<div class="col3">
-				<h3>Statistics</h3>
-				<ul>
-					<li><a href="index.php">none yet!</a></li>
-				</ul>
-			</div>
-
-			<div class="col3-center">
-				<h3>Graphics</h3>
-				<ul>
-					<li><a href="index.php">plot survey index correlations</a></li>
-				</ul>
-			</div>
-
-			<div class="col3">
-				<h3>Fun Stuff</h3>
-				<ul>
-					<li><a href="index.php">none yet</a></li>
-				</ul>
-			</div>
-		<!-- footer-columns ends -->
-		</div>
-
-		<div id="footer-bottom">
-			<p>
-			2009 <strong>Colin Millar</strong> |
-			Design by: <a href="http://www.styleshout.com/">styleshout</a> |
-			Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> |
-			<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
-
-   		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-			<a href="index.php">Home</a>&nbsp;|&nbsp;
-   		<a href="index.php">Sitemap</a>&nbsp;|&nbsp;
-	   	<a href="index.php">RSS Feed</a>
-   		</p>
-		</div>
-
-<!-- footer ends-->
-</div></div>
-
-<!-- wrap ends here -->
-</div>
+<p> <strong>Slides</strong> from short courses on lme4 <a href="./slides/"><strong>here</strong></a>. </p>
 
 </body>
 </html>
