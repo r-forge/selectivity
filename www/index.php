7101719d412b9773eff8d9a628c59de883220997
colinpmillar (Mr ) <colin.millar@jrc.ec.europa.eu>
2009-10-19 09:12:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/selectivity@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,51 +1,199 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
+<meta name="description" content="FRS Stats" />
+<meta name="keywords" content="R, statistics, graphics" />
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<meta name="Distribution" content="Global" />
+<meta name="author" content="Colin Millar" />
+<meta name="robots" content="index, follow" />
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-<?php
+<link rel="stylesheet" href="images/TechJunkie.css" type="text/css" />
 
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
+<title>FRS Stats</title>
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
+</head>
 
 <body>
+<!-- wrap starts here -->
+<div id="wrap">
 
-<!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+	<!--header -->
+	<div id="header">
+
+		<h1 id="logo-text"><a href="index.php" title="">FRS Stats</a></h1>
+		<p id="slogan">R package of FRS statistics group plotting functions</p>
+
+		<form id="quick-search" action="index.php" method="get" >
+			<p>
+			<label for="qsearch">Search:</label>
+			<input class="tbox" id="qsearch" type="text" name="qsearch" value="Search..." title="Start typing and hit ENTER" />
+			<input class="btn" alt="Search" type="image" name="searchsubmit" title="Search" src="images/search.gif" />
+			</p>
+		</form>
+
+	<!--header ends-->
+	</div>
+
+	<!-- navigation starts-->
+	<div  id="nav">
+		<ul>
+			<li id="current"><a href="index.php">Home</a></li>
+			<li><a href="http://cran.r-project.org/">CRAN</a></li>
+			<li><a href="http://r-forge.r-project.org/">R-Forge</a></li>
+			<li><a href="http://www.marlab.ac.uk/">FRS Homepage</a></li>
+			<li><a href="index.php">About</a></li>
+			<li><a href="index.php">Contact</a></li>
+		</ul>
+	<!-- navigation ends-->
+	</div>
+
+	<!-- content-wrap starts -->
+	<div id="content-wrap">
+
+		<div id="main">
+
+			<a name="Welcome"></a>
+			<h2><a href="index.php" title="">Welcome to the FRS Stats project on R-Forge!</a></h2>
+
+			<p>Some information on whatwe are trying to achieve witht this package...</p>
 
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+			<h2><a href="index.php">plot survey index correlations</a></h2>
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+			<h3>Plots - say what it does</h3>
+			<p><a href="http://getfirefox.com/"><img src="images/catch_correlations.png" width="100" height="120" alt="catch correlations" class="float-left" /></a>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum
+			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra
+			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna.
+			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
+			posuere nunc justo tempus leo.
+			</p>
 
-<!-- end of project description -->
+			<blockquote>
+			<p>Something interesting form someone equally interesting</p>
+			<p class="align-right"> - Jimmy Beaver</p>
+			</blockquote>
 
-<p> No content added. </p>
+			<p>
+			Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit amet bibendum
+			pellentesque, ante sapien varius turpis, eu nonummy nunc urna scelerisque ante.
+			Nunc sed leo at dolor pharetra facilisis. Sed sed lectus at justo dapibus tristique.
+			Morbi molestie pulvinar elit. Donec dictum nibh nec est. Aliquam sagittis, lacus
+			a pharetra tempor, enim justo tempor nisi, at aliquet nulla metus sed augue.
+			Praesent sed nibh. Vestibulum bibendum enim ut nisi fringilla accumsan.
+			Nullam dapibus fermentum nulla. Phasellus in orci vel justo facilisis iaculis.
+			Quisque facilisis ornare felis. Curabitur nunc libero, dapibus vel, commodo nec,
+			semper a, massa. Suspendisse potenti. Morbi a nisl. Nunc euismod elit non lectus.
+			</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+			<h2><a href="index.php">Another plot</a></h2>
+
+			<h3>Say what this one does</h3>
+			<p><a href="http://getfirefox.com/"><img src="images/firefox-gray.jpg" width="100" height="120" alt="firefox" class="float-right" /></a>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum
+			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra
+			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna.
+			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu
+			posuere nunc justo tempus leo.
+			</p>
+
+		<!-- main ends -->
+		</div>
+
+		<div id="sidebar">
+
+			<h3>FRS Stats Links</h3>
+			<ul class="sidemenu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="http://r-forge.r-project.org/projects/ices-frs/">FRS Stats R-Forge site</a></li>
+				<li><a href="http://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/?root=ices-frs">Browse FRS Stats Subversion Repository</a></li>
+			</ul>
+
+			<h3>Search Box</h3>
+			<form class="searchform" action="index.php" method="get">
+				<p>
+				<input name="search_query" class="textbox" type="text" />
+  				<input name="search" class="button" value="Search" type="submit" />
+				</p>
+			</form>
+
+			<h3>Wise Words</h3>
+			<p>&quot;Good judgement comes from experience; and experience comes from
+               bad judgement.&quot; </p>
+
+			<p class="align-right">- Mark Twain</p>
+
+			<h3>Support Styleshout</h3>
+
+      <p>This page was based on a design by <a href="http://www.styleshout.com/">Styleshout</a>.
+      If you are interested in supporting the work of Styleshout and would like to contribute, you are
+			welcome to make a small donation through the
+			<a href="http://www.styleshout.com/">donate link</a> on the Styleshout website - it will
+			be a great help and will surely be appreciated.</p>
+
+		<!-- sidebar ends -->
+		</div>
+
+	<!-- content-wrap ends-->
+	</div>
+
+	<!-- footer starts -->
+	<div id="footer-wrap"><div id="footer-content">
+
+		<div id="footer-columns">
+			<div class="col3">
+				<h3>Statistics</h3>
+				<ul>
+					<li><a href="index.php">none yet!</a></li>
+				</ul>
+			</div>
+
+			<div class="col3-center">
+				<h3>Graphics</h3>
+				<ul>
+					<li><a href="index.php">plot survey index correlations</a></li>
+				</ul>
+			</div>
+
+			<div class="col3">
+				<h3>Fun Stuff</h3>
+				<ul>
+					<li><a href="index.php">none yet</a></li>
+				</ul>
+			</div>
+		<!-- footer-columns ends -->
+		</div>
+
+		<div id="footer-bottom">
+			<p>
+			2009 <strong>Colin Millar</strong> |
+			Design by: <a href="http://www.styleshout.com/">styleshout</a> |
+			Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> |
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+
+   		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+			<a href="index.php">Home</a>&nbsp;|&nbsp;
+   		<a href="index.php">Sitemap</a>&nbsp;|&nbsp;
+	   	<a href="index.php">RSS Feed</a>
+   		</p>
+		</div>
+
+<!-- footer ends-->
+</div></div>
+
+<!-- wrap ends here -->
+</div>
 
 </body>
 </html>
