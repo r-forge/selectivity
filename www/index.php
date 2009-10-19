551fd1ad2461b47991456f1e87cc6c1d75998e70
colinpmillar (Mr ) <colin.millar@jrc.ec.europa.eu>
2009-10-19 15:58:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/selectivity@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en   "
-xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+
 <head>
-
- <meta name="description" content="Selectivity" />
- <meta name="keywords" content="R, statistics, graphics" />
- <meta http-equiv="content-type" content="text/xhtml; charset=UTF-8" />
- <meta name="Distribution" content="Global" />
- <meta name="author" content="Colin Millar" />
- <link rel="icon" href="favicon.ico" type="image/x-icon">
- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
- <link href="estilo1.css" rel="stylesheet" type="text/css" />
-
+ <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
  <title>Selectivity</title>
-
+ <link rel="icon" href="favicon.ico" type="image/x-icon" />
+ <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+ <link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
